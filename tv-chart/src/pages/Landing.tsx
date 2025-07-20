ab63b19d1b1e74ad9985effb67c7fb259be126b0
Yevhenii Romanenko <zerom2016romanenko@gmail.com>
2025-07-20 16:46:33 +0300

    add reactbits components

--- tv-chart/src/pages/Landing.tsx
+++ tv-chart/src/pages/Landing.tsx
@@ -1,15 +1,27 @@
+import FuzzyText from "../reactbits/TextAnimations/FuzzyText/FuzzyText"
+
 export const Landing = () => {
     return (
         <>
             <header>
                 <div className="brand-container">
-                    <span className="brand-name">VeritasX</span>
-                    <div className="brand-divider"></div>
-                    <span className="brand-subtitle">Crypto Charts</span>
+                    <FuzzyText
+                        hoverIntensity={0.4}
+                        color="#00d4aa"
+                        fontWeight={900}
+                        >
+                        VeritasX</FuzzyText>
                 </div>
             </header>
             <div className="brand-container">
-                <span className="brand-subtitle">build and test your technical ideas</span>
+                <FuzzyText
+                    fontSize={"2rem"}
+                    hoverIntensity={0.2}
+                    baseIntensity={0.05}
+                    color="rgba(224, 230, 237, 0.8)"
+                    fontWeight={900}
+                >
+                    BUILD AND TEST YOUR TECHICAL IDEAS</FuzzyText>
             </div>
         </>
     )
