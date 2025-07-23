ac536753ab957c34054cbbcc1c2f0149fc54c8d2
ggeirby <roromanenko2004@gmail.com>
2025-07-23 22:53:06 +0300

    Update max candles per request

--- VeritasX/Core/Options/DataCollectorOptions.cs
+++ VeritasX/Core/Options/DataCollectorOptions.cs
@@ -2,7 +2,7 @@ namespace VeritasX.Core.Options;
 
 public class DataCollectorOptions
 {
-    public int MaxCandlesPerRequest { get; set; } = 1000;
+    public int MaxCandlesPerRequest { get; set; } = 500;
     public int RequestsPerMinute { get; set; } = 1000;
     public int RetryAttempts { get; set; } = 3;
     public int BatchSize { get; set; } = 500;
